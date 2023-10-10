4be8a44e563809bc3a795199cb04d0be32465b49
NessaQuezada <122851420+NessaQuezada@users.noreply.github.com>
2023-10-09 22:34:04 -0700

    Add files via upload

---
+++ it162/includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <title>Vanessa Garcia's IT161 Portal Website</title>
+  <meta name="robots" content="noindex,nofollow" />
+  <meta name="viewport" content="width=device-width" />
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="css/portal.css" />
+  <link rel="stylesheet" href="css/nav.css" />
+ </head>
+ <body>
+     <header>
+     <h1>Vanessa's IT161 Website</h1>
+     <nav class="topnav" id="myTopnav">
+       <a href="index.php" class="active">Welcome</a>
+       <a href="elements.php">HTML Elements</a>
+       <a href="aia.php">AIA</a>
+       <a href="big/index.php">BIG</a>
+       <a href="http://w3schools.com" target="_blank">W3 Schools</a>
+       <a href="https://www.linkedin.com/learning/" target="_blank">LinkedIn Learning</a>
+       <a href="flowchart.php">Flowchart</a>
+       <a href="fp/index.html">Final Project</a>
+       <a href="contact.php">Contact Vanessa</a>
+       <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+     </nav>
+     </header>
+     
+   <div class="wrapper">
+     <!-- header ends here -->
